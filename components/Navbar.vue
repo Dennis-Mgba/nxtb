fd30cfb58764eae61839279d93f215e7e3bb7ce4
Dennis Mgbah <dennismgbah@gmail.com>
2020-02-09 19:13:30 +0100

    used nuxt link tag to link the pages in the application to each other in place of regular html anchor tag - navbar component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,16 +1,16 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Nblog</a>
+  <nuxt-link to="/" class="navbar-brand">Nblog</nuxt-link>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home</a>
+        <nuxt-link to="/" class="nav-link">Home</nuxt-link>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Users</a>
+        <nuxt-link to="/users" class="nav-link">Users</nuxt-link>
       </li>
     </ul>
   </div>
