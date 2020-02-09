17fa26fdb3cae30ea68a4639a9960d2f18105bc9
Dennis Mgbah <dennismgbah@gmail.com>
2020-02-09 19:00:22 +0100

    learnt what nuxt child is about - user file located in the base pages folder

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -3,3 +3,16 @@
       <h1>This is a unique user id = {{$route.params.id}}</h1>
   </div>
 </template>
+<script>
+export default {
+// the context here is an object and can help us access the route parameter.
+    // validate(context) {
+    //     // using a regular expression
+    //     return /^\d+$/.test(context.params.id)
+    // }
+
+    validate({params}) {
+        return /^\d+$/.test(params.id)
+    }
+}
+</script>
