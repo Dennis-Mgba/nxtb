b38b4c1d6dfadaecaa819f91f5c814f39cb65c0b
Dennis Mgbah <dennismgbah@gmail.com>
2020-02-09 18:03:50 +0100

    create a form on the users index file and wrote a code so that when an id is entered in the input, it will take the user to the route id.

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -2,23 +2,22 @@
     <div>
         <h1>User Page</h1>
 
-        <!-- <input type="text" v-model="id"/>     -->
-        <!--get the input data called id-->
-        <!-- <button @click="loadUserId">Load User</button> -->
+        <input type="text" v-model="id"/>    <!--get the input data called id-->
+        <button @click="loadUserId">Load User</button>
     </div>
 </template>
 
 <script>
-    // export default {
-    //     data() {
-    //         return {
-    //             id: ''       // declare the variale id as empty
-    //         }
-    //     },
-    //     methods: {
-    //         loadUserId() {
-    //             this.$router.push(`/users/${this.id}`)    // code to push the id data entered into the input to the empty id variable and use it on the route
-    //         }
-    //     }
-    // }
+    export default {
+        data() {
+            return {
+                id: ''       // declare the variale id as empty
+            }
+        },
+        methods: {
+            loadUserId() {
+                this.$router.push(`/users/${this.id}`)    // code to push the id data entered into the input to the empty id variable and use it on the route
+            }
+        }
+    }
 </script>
