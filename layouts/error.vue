00b5e2dd8981069a4fbfe1302e0a2177caee4aee
Dennis Mgbah <dennismgbah@gmail.com>
2020-02-09 19:19:58 +0100

    created own custom error file in the layouts folder - when a user request a page that is not in our application

---
+++ layouts/error.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        <h2>404 <br/> This page is not found in the application!</h2>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    text-align: center;
+}
+</style>
