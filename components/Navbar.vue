a1c64a8f1aa2ecc2b2392f69c77d31e250fa1d18
Dennis Mgbah <dennismgbah@gmail.com>
2020-02-14 06:22:29 +0100

    set nav active css

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,7 +6,7 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
+      <li class="nav-item">
         <nuxt-link to="/" class="nav-link">Home</nuxt-link>
       </li>
       <li class="nav-item">
@@ -19,3 +19,21 @@
   </div>
 </nav>
 </template>
+
+<style scoped>
+.navbar-dark .navbar-nav .nav-link {
+    color: #fff;
+}
+
+/* .nav-link a:hover,
+.nav-link a:active,
+.nav-link.nuxt-link-exact-active a {
+    color: #06C4D1;
+} */
+
+
+.navbar-dark .navbar-nav .nav-link:hover {
+    color: #06C4D1;
+    border-bottom: 3px solid #06C4D1;
+}
+</style>
